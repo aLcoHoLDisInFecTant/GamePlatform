65b96bb6ed5bdd6692696acca9ecb84749bbff9c
xiangyuwang <Xiangyu.Wang21@student.xjtlu.edu.cn>
2024-03-26 12:45:41 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+
+
+from PySide6.QtWidgets import QApplication, QWidget
+
+from Frontend_main_ui import Ui_Form
+
+
+
+class MyWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MyWidget()
+    window.show()
+
+    sys.exit(app.exec_())
